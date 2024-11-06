9b13fa8021a57aedf729a2789c815f54fccd1753
Saidou25 <115666389+Saidou25@users.noreply.github.com>
2024-11-06 16:04:03 -0500

    polished

--- src/components/Embed.js
+++ src/components/Embed.js
@@ -10,60 +10,71 @@
   margin: auto;
 }
 .finish-titles {
-    color: #615f5c;
+    // color: #615f5c;
+    color: black;
   text-align: center;
-  margin: 5% 0 10% 0; /* top, right, bottom, left */
-  font-family: sans-serif;
+  font-family: Anton, sans-serif;
+  letter-spacing: 1px;
+  font-size: 48px;
 }
 .score-today {
     text-align: center;
-    margin-bottom: 4%;
-    color: #615f5c;
+    margin: 4%;
+    color: black;
+    font-family: Poppins, sans-serif;
+    // color: #615f5c;
 }
 .results-table {
   margin: auto;
   border-collapse: separate;
   border-spacing: 0;
   border: 9px solid black;
-  width:69%;
+  width:66%;
 }
-.class-tr-top {
-    background-color: #aeb5b0
+.class-top {
+    background-color: #c7cec9;
 }
-.class-tr-bottom {
-    background-color: #fefefe;
+.class-bottom {
+    background-color: #fdfdfd;
 }
 .class-th {
   border-bottom: 1px solid grey;
   border-color: grey;
   font-family: Anton, sans-serif;
-  font-size: 24px;
+  text-rendering: optimizeLegibility;
+  font-size: 26px;
   color: black;
-  height: 50px;
   text-align: center;
-  padding: 2%;
-  width: 33%;
+  padding: 1% 7% 1% 7%;
+  width: 22%;
+  letter-spacing: 1px;
 }
 .class-td {
   border-right: 2px solid #c3c2c2;
+  border-bottom: 1px solid #c3c2c2;
   text-align: center;
-  width: 33%;
+  width: 22%;
   font-family: sans-serif;
   font-size: 28px;
+height: 50px;
 //   color: #615f5c;
-color: black;
+  color: black;
 }
 .class-td-perspective {
+  border-bottom: 1px solid #c3c2c2;
   height: 100px;
   text-align: center;
-  width: 33%;
+  width: 22%;
   font-family: sans-serif;
+  font-size: 28px;
+  height: 50px;
 //   color: #615f5c;
 color: black;
 }
 .thank-you {
     text-align: center;
-    margin: 10%;
+    margin: 3% 3%;
+    font-family: Poppins, sans serif;
 }
     // css for Questions
 .main-container {
@@ -91,13 +102,16 @@ color: black;
   justify-content: center;
 }
 .container-illustration {
-  max-width: 60%;
+  width: 200px;
+//   max-width: 90%;
   margin: auto;
-  text-align: center;
+//   text-align: center;
 }
 .illustration {
   max-width: 100%;
-  height: auto;
+//   height: auto;
+  background-position: cover;
+  aspect-ratio: 1 / 1;
 }
 .please-rate {
   color: #615f5c;
@@ -211,7 +225,6 @@ color: black;
   padding: 10px;
   color: white;
   letter-spacing: 2px;
-  margin-bottom: 7%;
 }
 
 
@@ -288,275 +301,7 @@ const questionsData = [
     userSelfRating: "",
     illustration: "https://assets.zyrosite.com/mP47Mwo0WQhVBkl5/image3-A85wVVXgRJfLBlXR.jpg",
   },
-  {
-    questionNumber: 4,
-    questionStatment:
-      "I live harmoniously in balance with myself and all of Nature.",
-    userSelfRating: "",
-    illustration: "https://assets.zyrosite.com/mP47Mwo0WQhVBkl5/4-ALpnPeqrVXs9BXny.png",
-  },
-  {
-    questionNumber: 5,
-    questionStatment:
-      "I am living fully in the present moment, open to all possibilities.",
-    userSelfRating: "",
-    noResponse: "",
-  },
-  {
-    questionNumber: 6,
-    questionStatment:
-      "I am deeply connected with my Spirit energy, which protects and guides me.",
-    userSelfRating: "",
-    noResponse: "",
-  },
-  {
-    questionNumber: 7,
-    questionStatment:
-      "I generate heart energy to experience greater love, joy, compassion, forgiveness, freedom and peace.",
-    userSelfRating: "",
-    noResponse: "",
-  },
-  {
-    questionNumber: 8,
-    questionStatment:
-      "The stronger I make my body the more energy I can hold and emit.",
-    userSelfRating: "",
-    noResponse: "",
-  },
-  {
-    questionNumber: 9,
-    questionStatment: "My mind is filled with positive thoughts.",
-    userSelfRating: "",
-    noResponse: "",
-  },
-  {
-    questionNumber: 10,
-    questionStatment: "I respect myself, everyone, and everything.",
-    userSelfRating: "",
-    illustration: "./src/assets/images/10.jpg",
-  },
-  {
-    questionNumber: 11,
-    questionStatment: "I continue to learn throughout my lifetime.",
-    userSelfRating: "",
-    illustration: "./src/assets/images/11.jpg",
-  },
-  {
-    questionNumber: 12,
-    questionStatment:
-      "I rely on my wisdom to make conscious choices for my life.",
-    userSelfRating: "",
-    noResponse: "",
-  },
-  {
-    questionNumber: 13,
-    questionStatment: "I am fulfilled by serving my life purpose.",
-    userSelfRating: "",
-    illustration: "./src/assets/images/13.jpg",
-  },
-  {
-    questionNumber: 14,
-    questionStatment:
-      "I am open to, and accept, all changes that support my life purpose.",
-    userSelfRating: "",
-    noResponse: "",
-  },
-  {
-    questionNumber: 15,
-    questionStatment: "I accept what is, as it is, when it is.",
-    userSelfRating: "",
-    noResponse: "",
-  },
-  {
-    questionNumber: 16,
-    questionStatment:
-      "I live a heart-centered life, making heart-centered decisions.",
-    userSelfRating: "",
-    noResponse: "",
-  },
-  {
-    questionNumber: 17,
-    questionStatment:
-      "I forgive myself and others, as we are all teachers for one another. Forgiveness is my path to freedom.",
-    userSelfRating: "",
-    noResponse: "",
-  },
-  {
-    questionNumber: 18,
-    questionStatment:
-      "I AM whole, fully embracing all Aspects of my Self, including my shadows.",
-    userSelfRating: "",
-    noResponse: "",
-  },
-  {
-    questionNumber: 19,
-    questionStatment: "I unconditionally love myself.",
-    userSelfRating: "",
-    illustration: "./src/assets/images/19.jpg",
-  },
-  {
-    questionNumber: 20,
-    questionStatment: "I live my life with a pure and honest heart.",
-    userSelfRating: "",
-    noResponse: "",
-  },
-  {
-    questionNumber: 21,
-    questionStatment: "I inspire others by being my authentic self.",
-    userSelfRating: "",
-    noResponse: "",
-  },
-  {
-    questionNumber: 22,
-    questionStatment: "I live my truth, as it protects me.",
-    userSelfRating: "",
-    noResponse: "",
-  },
-  {
-    questionNumber: 23,
-    questionStatment: "I trust myself and others.",
-    userSelfRating: "",
-    noResponse: "",
-  },
-  {
-    questionNumber: 24,
-    questionStatment: "I am clear and confident, humble and bold.",
-    userSelfRating: "",
-    noResponse: "",
-  },
-  {
-    questionNumber: 25,
-    questionStatment:
-      "I experience inner peace as a result of doing my inner work.",
-    userSelfRating: "",
-    noResponse: "",
-  },
-  {
-    questionNumber: 26,
-    questionStatment: "I surrender my personal will to Divine will.",
-    userSelfRating: "",
-    illustration: "./src/assets/images/26.jpg",
-  },
-  {
-    questionNumber: 27,
-    questionStatment:
-      "I listen to the rhythm of my Soul. And I trust the Creator’s pace.",
-    userSelfRating: "",
-    illustration: "./src/assets/images/27.jpg",
-  },
-  {
-    questionNumber: 28,
-    questionStatment: "I live my life with bravery.",
-    userSelfRating: "",
-    illustration: "./src/assets/images/28.jpg",
-  },
-  {
-    questionNumber: 29,
-    questionStatment:
-      "I will continue to persevere on my journey until I manifest my true heart’s desires and fulfill my life purpose.",
-    userSelfRating: "",
-    illustration: "./src/assets/images/29.jpg",
-  },
-  {
-    questionNumber: 30,
-    questionStatment:
-      "I am at peace with myself, and grateful in every area of my life.",
-    userSelfRating: "",
-    illustration: "./src/assets/images/30.jpg",
-  },
-  {
-    questionNumber: 31,
-    questionStatment: "I am humble and kind.",
-    userSelfRating: "",
-    illustration: "./src/assets/images/31.jpg",
-  },
-  {
-    questionNumber: 32,
-    questionStatment: "I live a simple life with very little drama.",
-    userSelfRating: "",
-    illustration: "./src/assets/images/32.jpg",
-  },
-  {
-    questionNumber: 33,
-    questionStatment:
-      "I work in collaboration with others to help fuel our collective success.",
-    userSelfRating: "",
-    illustration: "./src/assets/images/33.jpg",
-  },
-  {
-    questionNumber: 34,
-    questionStatment:
-      "With vision, every human challenge is possible to overcome.",
-    userSelfRating: "",
-    illustration: "./src/assets/images/34.jpg",
-  },
-  {
-    questionNumber: 35,
-    questionStatment:
-      "My focus inspires me to manifest my vision into reality.",
-    userSelfRating: "",
-    illustration: "./src/assets/images/35.jpg",
-  },
-  {
-    questionNumber: 36,
-    questionStatment:
-      "I contemplate my thoughts before I act, choosing to make decisions that support my life.",
-    userSelfRating: "",
-    illustration: "./src/assets/images/36.jpg",
-  },
-  {
-    questionNumber: 37,
-    questionStatment: "I view everyone and everything as equals.",
-    userSelfRating: "",
-    illustration: "./src/assets/images/37.jpg",
-  },
-  {
-    questionNumber: 38,
-    questionStatment:
-      "I live my life with compassion for myself and others. I give to give, not give to get.",
-    userSelfRating: "",
-    illustration: "./src/assets/images/38.jpg",
-  },
-  {
-    questionNumber: 39,
-    questionStatment:
-      "I am an aspect of the togetherness that bonds our unified community.",
-    userSelfRating: "",
-    noResponse: "",
-  },
-  {
-    questionNumber: 40,
-    questionStatment: "I am free!",
-    userSelfRating: "",
-    illustration: "./src/assets/images/40.jpg",
-  },
-  {
-    questionNumber: 41,
-    questionStatment:
-      "I am fulfilled by giving to others. I am fulfilled by receiving from others.",
-    userSelfRating: "",
-    noResponse: "",
-  },
-  {
-    questionNumber: 42,
-    questionStatment: "I live a prosperous life.",
-    userSelfRating: "",
-    noResponse: "",
-  },
-  {
-    questionNumber: 43,
-    questionStatment:
-      "I send thoughts of love, and love returns in overflowing measure.",
-    userSelfRating: "",
-    illustration: "./src/assets/images/43.jpg",
-  },
-  {
-    questionNumber: 44,
-    questionStatment:
-      "I live in harmony with Nature, for I am Nature. Conscious and regenerative living aligns with who I am.",
-    userSelfRating: "",
-    noResponse: "",
-  },
+  
 ];
 
   function Finish({ userAnswers }) {
@@ -573,7 +318,7 @@ const questionsData = [
     const [buttonDisabled, setButtonDisabled] = React.useState(true);
     const [forwardDisabled, setForwardDisabled] = React.useState(true);
     const [userAnswers, setUserAnswers] = React.useState([]);
-    const [end, setEnd] = React.useState(true);
+    const [end, setEnd] = React.useState(false);
     const [isSelected, setIsSelected] = React.useState("");
     const [storedUserSelfRating, setStoredUserSelfRating] = React.useState("");
     const [formState, setFormState] = React.useState({
@@ -722,7 +467,8 @@ document.head.appendChild(link);
 console.log(buttonDisabled)
     if (end) {
         return React.createElement("div", { className: "finish-main-container" },
-            React.createElement("h1", { className: "finish-titles" }, "ESSA44 Test Results"),
+            React.createElement("h1", { className: "finish-titles" }, "ESSSENCE Self-Awarness ESA44"),
+            React.createElement("h1", { className: "finish-titles" }, "Test Results"),
             
             React.createElement("h2", { className: "score-today" },
               "Your Score for Today is: " + 
@@ -731,9 +477,8 @@ console.log(buttonDisabled)
             
             React.createElement("table", { className: "results-table" },
               React.createElement("thead", null,
-                React.createElement("tr", { className: "class-tr-top" },
-                //   React.createElement("th", { className: "class-th" }, "Date of Round"),
-                  React.createElement("th", { className: "class-th" }, "ESSA44 Awakening %"),
+                React.createElement("tr", { className: "class-top" },
+                  React.createElement("th", { className: "class-th" }, "ESA44 Awakening %"),
                   React.createElement("th", { className: "class-th" }, "State of Awareness"),
                   React.createElement("th", { className: "class-th" }, "Current Perspective")
                 )
@@ -743,8 +488,7 @@ console.log(buttonDisabled)
                 resultsData && resultsData.map((result, index) =>
                   totalAssessment >= result.minPercentage &&
                   totalAssessment <= result.maxPercentage &&
-                    React.createElement("tr", { key: index, className: "class-tr-bottom" },
-                    //   React.createElement("td", { className: "class-td" }, formattedDate),
+                    React.createElement("tr", { key: index, className: "class-bottom" },
                       React.createElement("td", { className: "class-td" }, result.percentage),
                       React.createElement("td", { className: "class-td" }, result.awakeness),
                       React.createElement("td", { className: "class-td-perspective" }, result.perspective)
@@ -752,10 +496,27 @@ console.log(buttonDisabled)
                 )
               )
             ),
-            React.createElement("h2", { className: "thank-you" }, "Thank you for completing our assessment.")
-            
+            React.createElement("h2", { className: "thank-you" }, "Thank you for completing ESA44 assessment."),
+            React.createElement("table", { className: "results-table" },
+                React.createElement("thead", null,
+                  React.createElement("tr", { className: "class-top" },
+                    React.createElement("th", { className: "class-th" }, "ESA44 Awakening %"),
+                    React.createElement("th", { className: "class-th" }, "State of Awareness"),
+                    React.createElement("th", { className: "class-th" }, "Current Perspective")
+                  )
+                ),
+                
+                React.createElement("tbody", null,
+                  resultsData && resultsData.map((result, index) =>
+                      React.createElement("tr", { key: index, className: "class-bottom" },
+                        React.createElement("td", { className: "class-td" }, result.percentage),
+                        React.createElement("td", { className: "class-td" }, result.awakeness),
+                        React.createElement("td", { className: "class-td-perspective" }, result.perspective)
+                      )
+                  )
+                )
+              ),          
           );
-          
     }
 
     return React.createElement(
