d5f745c2276f4ae4a588eed4594b1223e1c7dfb5
Saidou25 <115666389+Saidou25@users.noreply.github.com>
2024-11-13 18:17:58 -0500

    deploying

--- src/questionsData.js
+++ src/questionsData.js
@@ -6,6 +6,8 @@ export const resultsData = [
     percentage: "96-100%",
     awakeness: "Awake",
     perspective: "Awakened",
+    illustration:
+      "https://assets.zyrosite.com/mP47Mwo0WQhVBkl5/esa44-awareness-YlevljgyZ8ig1rbN.jpg",
   },
   {
     tile: "",
@@ -14,6 +16,8 @@ export const resultsData = [
     percentage: "81-95%",
     awakeness: "Almost Awake",
     perspective: "Seeker",
+    illustration:
+      "https://assets.zyrosite.com/mP47Mwo0WQhVBkl5/esa44-infinite-being-AzGeL9grl5cVnGJO.jpg",
   },
   {
     tile: "",
@@ -22,6 +26,8 @@ export const resultsData = [
     percentage: "61-80%",
     awakeness: "Awaking",
     perspective: "Seeker",
+    illustration:
+      "https://assets.zyrosite.com/mP47Mwo0WQhVBkl5/esa44-connectedness-A0xwOqzwW9hp0zky.jpg",
   },
   {
     tile: "",
@@ -30,6 +36,8 @@ export const resultsData = [
     percentage: "41-60%",
     awakeness: "Seeking",
     perspective: "Seeker",
+    illustration:
+      "https://assets.zyrosite.com/mP47Mwo0WQhVBkl5/esa44-balance-A0xwOqzwvGF2ELPX.jpghttps://assets.zyrosite.com/mP47Mwo0WQhVBkl5/esa44-present-time-dJo6LVQ6bnSG445a.jpg",
   },
   {
     tile: "",
@@ -38,6 +46,8 @@ export const resultsData = [
     percentage: "21-40%",
     awakeness: "Waking Up",
     perspective: "Sleeping Soul",
+    illustration:
+      "https://assets.zyrosite.com/mP47Mwo0WQhVBkl5/esa44-spirit-energy-mv0DBerDPDHVy628.jpg",
   },
   {
     tile: "",
@@ -46,6 +56,8 @@ export const resultsData = [
     percentage: "0-20%",
     awakeness: "Asleep",
     perspective: "Sleeping Soul",
+    illustration:
+      "https://assets.zyrosite.com/mP47Mwo0WQhVBkl5/esa44-heart-energy-m5KwEjzaqwcKrbeD.jpg",
   },
 ];
 
@@ -54,7 +66,8 @@ export const questionsData = [
     questionNumber: 1,
     questionStatment: "I Know Thyself… Spirit, Heart, Body, Mind, and Soul.",
     userSelfRating: "",
-    illustration: "./src/assets/images/1.jpg",
+    illustration:
+      "https://assets.zyrosite.com/mP47Mwo0WQhVBkl5/esa44-awareness-YlevljgyZ8ig1rbN.jpg",
   },
   {
     questionNumber: 2,
@@ -62,12 +75,15 @@ export const questionsData = [
       "I accept myself. I accept the infinite. I accept that I am infinite.",
     userSelfRating: "",
     noResponse: "",
+    illustration:
+      "https://assets.zyrosite.com/mP47Mwo0WQhVBkl5/esa44-infinite-being-AzGeL9grl5cVnGJO.jpg",
   },
   {
     questionNumber: 3,
     questionStatment: "I am connected as One with all aspects of the Universe.",
     userSelfRating: "",
-    illustration: "./src/assets/images/3.jpg",
+    illustration:
+      "https://assets.zyrosite.com/mP47Mwo0WQhVBkl5/esa44-connectedness-A0xwOqzwW9hp0zky.jpg",
   },
   {
     questionNumber: 4,
@@ -75,6 +91,8 @@ export const questionsData = [
       "I live harmoniously in balance with myself and all of Nature.",
     userSelfRating: "",
     noResponse: "",
+    illustration:
+      "https://assets.zyrosite.com/mP47Mwo0WQhVBkl5/esa44-balance-A0xwOqzwvGF2ELPX.jpg",
   },
   {
     questionNumber: 5,
@@ -82,6 +100,8 @@ export const questionsData = [
       "I am living fully in the present moment, open to all possibilities.",
     userSelfRating: "",
     noResponse: "",
+    illustration:
+      "https://assets.zyrosite.com/mP47Mwo0WQhVBkl5/esa44-present-time-dJo6LVQ6bnSG445a.jpg",
   },
   {
     questionNumber: 6,
@@ -89,6 +109,8 @@ export const questionsData = [
       "I am deeply connected with my Spirit energy, which protects and guides me.",
     userSelfRating: "",
     noResponse: "",
+    illustration:
+      "https://assets.zyrosite.com/mP47Mwo0WQhVBkl5/esa44-spirit-energy-mv0DBerDPDHVy628.jpg",
   },
   {
     questionNumber: 7,
@@ -96,6 +118,8 @@ export const questionsData = [
       "I generate heart energy to experience greater love, joy, compassion, forgiveness, freedom and peace.",
     userSelfRating: "",
     noResponse: "",
+    illustration:
+      "https://assets.zyrosite.com/mP47Mwo0WQhVBkl5/esa44-heart-energy-m5KwEjzaqwcKrbeD.jpg",
   },
   {
     questionNumber: 8,
@@ -103,24 +127,30 @@ export const questionsData = [
       "The stronger I make my body the more energy I can hold and emit.",
     userSelfRating: "",
     noResponse: "",
+    illustration:
+      "https://assets.zyrosite.com/mP47Mwo0WQhVBkl5/esa44-body-energy-Yyv3xBw1EphMDXDa.jpg",
   },
   {
     questionNumber: 9,
     questionStatment: "My mind is filled with positive thoughts.",
     userSelfRating: "",
     noResponse: "",
+    illustration:
+      "https://assets.zyrosite.com/mP47Mwo0WQhVBkl5/esa44-mind-energy-AR0yNZGX4jS4DMGX.jpg",
   },
   {
     questionNumber: 10,
     questionStatment: "I respect myself, everyone, and everything.",
     userSelfRating: "",
-    illustration: "./src/assets/images/10.jpg",
+    illustration:
+      "https://assets.zyrosite.com/mP47Mwo0WQhVBkl5/esa44-respect-mePvEe9oO9Twg0yj.jpg",
   },
   {
     questionNumber: 11,
     questionStatment: "I continue to learn throughout my lifetime.",
     userSelfRating: "",
-    illustration: "./src/assets/images/11.jpg",
+    illustration:
+      "https://assets.zyrosite.com/mP47Mwo0WQhVBkl5/esa44-lifelong-learner-mxBZO3JeXqIoXwJM.jpg",
   },
   {
     questionNumber: 12,
@@ -128,12 +158,15 @@ export const questionsData = [
       "I rely on my wisdom to make conscious choices for my life.",
     userSelfRating: "",
     noResponse: "",
+    illustration:
+      "https://assets.zyrosite.com/mP47Mwo0WQhVBkl5/esa44-wisdom-YrDlZPx51airPeWo.jpg",
   },
   {
     questionNumber: 13,
     questionStatment: "I am fulfilled by serving my life purpose.",
     userSelfRating: "",
-    illustration: "./src/assets/images/13.jpg",
+    illustration:
+      "https://assets.zyrosite.com/mP47Mwo0WQhVBkl5/esa44-life-purpose-m2WpngLXy4UVW1qN.jpg",
   },
   {
     questionNumber: 14,
@@ -141,12 +174,16 @@ export const questionsData = [
       "I am open to, and accept, all changes that support my life purpose.",
     userSelfRating: "",
     noResponse: "",
+    illustration:
+      "https://assets.zyrosite.com/mP47Mwo0WQhVBkl5/esa44-change-dJo6L7vgbZHb5bqw.jpg",
   },
   {
     questionNumber: 15,
     questionStatment: "I accept what is, as it is, when it is.",
     userSelfRating: "",
     noResponse: "",
+    illustration:
+      "https://assets.zyrosite.com/mP47Mwo0WQhVBkl5/esa44-acceptance-mxBZO3JNNGFBOj5k.jpg",
   },
   {
     questionNumber: 16,
@@ -154,6 +191,8 @@ export const questionsData = [
       "I live a heart-centered life, making heart-centered decisions.",
     userSelfRating: "",
     noResponse: "",
+    illustration:
+      "https://assets.zyrosite.com/mP47Mwo0WQhVBkl5/esa44-heart-centeredness-Y4LV5y7ByqIb8k19.jpg",
   },
   {
     questionNumber: 17,
@@ -161,6 +200,8 @@ export const questionsData = [
       "I forgive myself and others, as we are all teachers for one another. Forgiveness is my path to freedom.",
     userSelfRating: "",
     noResponse: "",
+    illustration:
+      "https://assets.zyrosite.com/mP47Mwo0WQhVBkl5/esa44-forgiveness-AE0onV1wgnHLQekO.jpg",
   },
   {
     questionNumber: 18,
@@ -168,42 +209,54 @@ export const questionsData = [
       "I AM whole, fully embracing all Aspects of my Self, including my shadows.",
     userSelfRating: "",
     noResponse: "",
+    illustration:
+      "https://assets.zyrosite.com/mP47Mwo0WQhVBkl5/esa44-wholeness-ALpn5V6yD8cG7MJ1.jpg",
   },
   {
     questionNumber: 19,
     questionStatment: "I unconditionally love myself.",
     userSelfRating: "",
-    illustration: "./src/assets/images/19.jpg",
+    illustration:
+      "https://assets.zyrosite.com/mP47Mwo0WQhVBkl5/esa44-self-love-d95KqvNevlupyXqj.jpg",
   },
   {
     questionNumber: 20,
     questionStatment: "I live my life with a pure and honest heart.",
     userSelfRating: "",
     noResponse: "",
+    illustration:
+      "https://assets.zyrosite.com/mP47Mwo0WQhVBkl5/esa44-honesty-A85wz3731nfZExyG.jpg",
   },
   {
     questionNumber: 21,
     questionStatment: "I inspire others by being my authentic self.",
     userSelfRating: "",
     noResponse: "",
+    illustration:
+      "https://assets.zyrosite.com/mP47Mwo0WQhVBkl5/esa44-authenticity-Y4LV5anyloU5a249.jpg",
   },
+
   {
     questionNumber: 22,
     questionStatment: "I live my truth, as it protects me.",
     userSelfRating: "",
     noResponse: "",
+    illustration: "",
   },
   {
     questionNumber: 23,
     questionStatment: "I trust myself and others.",
     userSelfRating: "",
     noResponse: "",
+    illustration: "",
   },
   {
     questionNumber: 24,
     questionStatment: "I am clear and confident, humble and bold.",
     userSelfRating: "",
     noResponse: "",
+    illustration:
+      "https://assets.zyrosite.com/mP47Mwo0WQhVBkl5/esa44-confidence-ALpn5lkbGjSyge69.jpg",
   },
   {
     questionNumber: 25,
@@ -211,105 +264,122 @@ export const questionsData = [
       "I experience inner peace as a result of doing my inner work.",
     userSelfRating: "",
     noResponse: "",
+    illustration:
+      "https://assets.zyrosite.com/mP47Mwo0WQhVBkl5/esa44-peacefulness-mk3z6EO8ROCNO8Zg.jpg",
   },
   {
     questionNumber: 26,
     questionStatment: "I surrender my personal will to Divine will.",
     userSelfRating: "",
-    illustration: "./src/assets/images/26.jpg",
+    illustration:
+      "https://assets.zyrosite.com/mP47Mwo0WQhVBkl5/esa44-divine-will-ALpn5lkbXafaBOZz.jpg",
   },
   {
     questionNumber: 27,
     questionStatment:
       "I listen to the rhythm of my Soul. And I trust the Creator’s pace.",
     userSelfRating: "",
-    illustration: "./src/assets/images/27.jpg",
+    illustration:
+      "https://assets.zyrosite.com/mP47Mwo0WQhVBkl5/esa44-patience-mePvENZLGOUq4Qax.jpg",
   },
   {
     questionNumber: 28,
     questionStatment: "I live my life with bravery.",
     userSelfRating: "",
-    illustration: "./src/assets/images/28.jpg",
+    illustration:
+      "https://assets.zyrosite.com/mP47Mwo0WQhVBkl5/esa44-bravery-YBgrZBklbbHBVrpq.jpg",
   },
   {
     questionNumber: 29,
     questionStatment:
       "I will continue to persevere on my journey until I manifest my true heart’s desires and fulfill my life purpose.",
     userSelfRating: "",
-    illustration: "./src/assets/images/29.jpg",
+    illustration:
+      "https://assets.zyrosite.com/mP47Mwo0WQhVBkl5/esa44-perseverance-AVLp8VkNR4FpLqn4.jpg",
   },
   {
     questionNumber: 30,
     questionStatment:
       "I am at peace with myself, and grateful in every area of my life.",
     userSelfRating: "",
-    illustration: "./src/assets/images/30.jpg",
+    illustration:
+      "https://assets.zyrosite.com/mP47Mwo0WQhVBkl5/esa44-gratitude-mp8vD0En84Iv3DeM.jpg",
   },
   {
     questionNumber: 31,
     questionStatment: "I am humble and kind.",
     userSelfRating: "",
-    illustration: "./src/assets/images/31.jpg",
+    illustration:
+      "https://assets.zyrosite.com/mP47Mwo0WQhVBkl5/esa44-humility-m5KwE79vgOCvxv4d.jpg",
   },
   {
     questionNumber: 32,
     questionStatment: "I live a simple life with very little drama.",
     userSelfRating: "",
-    illustration: "./src/assets/images/32.jpg",
+    illustration:
+      "https://assets.zyrosite.com/mP47Mwo0WQhVBkl5/esa44-simplicity-AoPv2k5DOqFkZ4Pn.jpg",
   },
   {
     questionNumber: 33,
     questionStatment:
       "I work in collaboration with others to help fuel our collective success.",
     userSelfRating: "",
-    illustration: "./src/assets/images/33.jpg",
+    illustration:
+      "https://assets.zyrosite.com/mP47Mwo0WQhVBkl5/esa44-collaboration-mjEvNWaQ6RU10oW2.jpg",
   },
   {
     questionNumber: 34,
     questionStatment:
       "With vision, every human challenge is possible to overcome.",
     userSelfRating: "",
-    illustration: "./src/assets/images/34.jpg",
+    illustration:
+      "https://assets.zyrosite.com/mP47Mwo0WQhVBkl5/esa44-vision-YKb6j9kJNBSGyxGq.jpg",
   },
   {
     questionNumber: 35,
     questionStatment:
       "My focus inspires me to manifest my vision into reality.",
     userSelfRating: "",
-    illustration: "./src/assets/images/35.jpg",
+    illustration:
+      "https://assets.zyrosite.com/mP47Mwo0WQhVBkl5/esa44-focus-YZ9VZ4kXJJC9wWbw.jpg",
   },
   {
     questionNumber: 36,
     questionStatment:
       "I contemplate my thoughts before I act, choosing to make decisions that support my life.",
     userSelfRating: "",
-    illustration: "./src/assets/images/36.jpg",
+    illustration:
+      "https://assets.zyrosite.com/mP47Mwo0WQhVBkl5/esa44-contemplation-Yanyxj8z2NFoGRPr.jpg",
   },
   {
     questionNumber: 37,
     questionStatment: "I view everyone and everything as equals.",
     userSelfRating: "",
-    illustration: "./src/assets/images/37.jpg",
+    illustration:
+      "https://assets.zyrosite.com/mP47Mwo0WQhVBkl5/esa44-equality-Yg2Wvxel23hWNQzb.jpg",
   },
   {
     questionNumber: 38,
     questionStatment:
       "I live my life with compassion for myself and others. I give to give, not give to get.",
     userSelfRating: "",
-    illustration: "./src/assets/images/38.jpg",
+    illustration:
+      "https://assets.zyrosite.com/mP47Mwo0WQhVBkl5/esa44-compassion-m7Vwvo4kQPFX97LJ.jpg",
   },
   {
     questionNumber: 39,
     questionStatment:
       "I am an aspect of the togetherness that bonds our unified community.",
-    userSelfRating: "",
     noResponse: "",
+    illustration:
+      "https://assets.zyrosite.com/mP47Mwo0WQhVBkl5/esa44-unity-mxBZOkn8eGFPaXKD.jpg",
   },
   {
     questionNumber: 40,
     questionStatment: "I am free!",
     userSelfRating: "",
-    illustration: "./src/assets/images/40.jpg",
+    illustration:
+      "https://assets.zyrosite.com/mP47Mwo0WQhVBkl5/esa44-freedom-m5KwE79eXzCJ28Qo.jpg",
   },
   {
     questionNumber: 41,
@@ -317,19 +387,25 @@ export const questionsData = [
       "I am fulfilled by giving to others. I am fulfilled by receiving from others.",
     userSelfRating: "",
     noResponse: "",
+    illustration: "",
   },
   {
     questionNumber: 42,
     questionStatment: "I live a prosperous life.",
     userSelfRating: "",
+    illustration:
+      "https://assets.zyrosite.com/mP47Mwo0WQhVBkl5/esa44-giving-receiving-mp8vD0EXD9FRrR5O.jpg",
     noResponse: "",
+    illustration:
+      "https://assets.zyrosite.com/mP47Mwo0WQhVBkl5/esa44-prosperity-m5KwE79rL5FGpzyZ.jpg",
   },
   {
     questionNumber: 43,
     questionStatment:
       "I send thoughts of love, and love returns in overflowing measure.",
     userSelfRating: "",
-    illustration: "./src/assets/images/43.jpg",
+    illustration:
+      "https://assets.zyrosite.com/mP47Mwo0WQhVBkl5/esa44-unconditional-love-A0xwOQJWqyIyxX1R.jpg",
   },
   {
     questionNumber: 44,
@@ -337,6 +413,7 @@ export const questionsData = [
       "I live in harmony with Nature, for I am Nature. Conscious and regenerative living aligns with who I am.",
     userSelfRating: "",
     noResponse: "",
+    illustration:
+      "https://assets.zyrosite.com/mP47Mwo0WQhVBkl5/esa44-conscious-regenerative-living-YZ9VZ4klkEFoKggj.jpg",
   },
 ];
-
