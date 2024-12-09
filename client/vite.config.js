1d2c9d2a0e26943f4c5050871ebebe53284f5349
Saidou25 <115666389+Saidou25@users.noreply.github.com>
2024-12-09 08:04:18 -0500

    changed the tag image

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "./",
+  build: {
+    assetsInlineLimit: 0, // Ensures assets like images are not inlined
+  },
 });
