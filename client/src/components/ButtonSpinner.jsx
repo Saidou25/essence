5087d80b6d714432fafa66424ba0ba500858bca6
Saidou25 <115666389+Saidou25@users.noreply.github.com>
2024-12-12 09:52:23 -0500

    adjusted email form font-size @media screen and button jumps

--- client/src/components/ButtonSpinner.jsx
+++ client/src/components/ButtonSpinner.jsx
@@ -3,7 +3,6 @@ import "./ButtonSpinner.css";
 const ButtonSpinner = () => {
   return (
     <button className="spinner-button">
-    {/* <span className="button-text">Submit</span> */}
     <span className="spinner"></span>
   </button>
   
